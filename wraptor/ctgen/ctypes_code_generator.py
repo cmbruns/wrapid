4d72a568ede1b8e1612c633e8f79fe28bdc6fc66
Christopher Bruns <cmbruns@rotatingpenguin.com>
2024-07-10 10:29:35 -0700

    Move away from using Declaration wrapper classes in ctypes_code_generator. Track ctypes imported symbols.

--- wraptor/ctgen/ctypes_code_generator.py
+++ wraptor/ctgen/ctypes_code_generator.py
@@ -1,6 +1,9 @@
+from typing import Optional
+
+import clang.cindex
+from clang.cindex import Cursor, CursorKind, TypeKind
+
 from wraptor import ModuleBuilder
-from wraptor.decl.struct import StructDeclaration
-from wraptor.decl.typedef import TypeDefDeclaration
 
 
 class CTypesCodeGenerator(object):
@@ -8,39 +11,115 @@ class CTypesCodeGenerator(object):
         self.module_builder = module_builder
 
     def write_module(self, file):
+        module_index = ModuleIndex()
+        body_lines = []
         for decl in self.module_builder.declarations():
             if not decl.included():
                 continue
-            coder = coder_map[type(decl)]
-            for line in coder(decl, 0):
-                print(line, file=file)
+            coder = coder_for_cursor_kind[decl.cursor.kind]
+            for line in coder(decl.cursor, 0, module_index):
+                body_lines.append(line)
+        for line in module_index.import_code():
+            print(line, file=file)
+        for line in body_lines:
+            print(line, file=file)
 
 
-def field_code(field, indent=0):
+class ModuleIndex(object):
+    """
+    Tracks needed import statements and declaration dependencies (eventually...)
+    """
+    def __init__(self):
+        self.imports = dict()
+
+    def set_import(self, import_module: str, import_name: str):
+        """
+        Track import statements needed for the python module we are creating
+        """
+        if import_module not in self.imports:
+            self.imports[import_module] = set()
+        self.imports[import_module].add(import_name)
+
+    def import_code(self):
+        for module in self.imports:
+            yield f"from {module} import {', '.join(self.imports[module])}"
+
+
+def ctypes_name_for_cursor(cursor: Cursor):
+    if cursor.kind == CursorKind.STRUCT_DECL:
+        # Workaround for anonymous structs
+        if len(cursor.spelling) < 1:
+            return cursor.type.spelling
+    return cursor.spelling
+
+
+def ctypes_name_for_clang_type(clang_type: clang.cindex.Type, module_index=None):
+    """
+    Recursive function to build up complex type names piece by piece
+    """
+    # TODO: maybe replace wraptor type_for_clang_type
+    if clang_type.kind == TypeKind.CONSTANTARRAY:
+        element_type = ctypes_name_for_clang_type(clang_type.element_type, module_index)
+        # TODO: element_count may or may not be a constant macro
+        return f"{element_type} * {clang_type.element_count}"
+    elif clang_type.kind == TypeKind.ELABORATED:
+        return ctypes_name_for_clang_type(clang_type.get_declaration().type, module_index)
+    elif clang_type.kind == TypeKind.FUNCTIONPROTO:
+        result_type = ctypes_name_for_clang_type(clang_type.get_result(), module_index)
+        arg_types = [ctypes_name_for_clang_type(a, module_index) for a in clang_type.argument_types()]
+        index_import(module_index, "ctypes", "CFUNCTYPE")
+        return f"CFUNCTYPE({result_type}, {', '.join(arg_types)})"
+    elif clang_type.kind == TypeKind.POINTER:
+        pointee = clang_type.get_pointee()
+        index_import(module_index, "ctypes", "POINTER")
+        return f"POINTER({ctypes_name_for_clang_type(pointee, module_index)})"
+    else:
+        return clang_type.spelling
+
+
+def field_code(field_cursor: Cursor, indent=8, module_index=None):
     i = indent * " "
-    yield i+f'("{field.name}", {field.cursor.type.spelling}),'  # TODO: more work on types
+    assert field_cursor.kind == CursorKind.FIELD_DECL
+    field_name = ctypes_name_for_cursor(field_cursor)
+    type_name = ctypes_name_for_clang_type(field_cursor.type, module_index)
+    yield i+f'("{field_name}", {type_name}),'  # TODO: more work on types
 
 
-def struct_code(struct: StructDeclaration, indent=0):
+def index_import(module_index: Optional[ModuleIndex], import_module: str, import_name: str):
+    if module_index is None:
+        return
+    module_index.set_import(import_module, import_name)
+
+
+def struct_code(cursor: Cursor, indent=0, module_index=None):
+    assert cursor.kind == CursorKind.STRUCT_DECL
     i = indent * " "
-    yield i+f"class {struct.name}(ctypes.Structure):"
-    if len(struct.fields) > 0:
+    name = ctypes_name_for_cursor(cursor)
+    yield i+f"class {name}(Structure):"
+    index_import(module_index, "ctypes", "Structure")
+    fields = []
+    for child in cursor.get_children():
+        if child.kind == CursorKind.FIELD_DECL:
+            fields.append(child)
+    if len(fields) > 0:
         yield i+f"    _fields_ = ("
-        for field in struct.fields:
-            yield from field_code(field, indent + 8)
+        for field in fields:
+            yield from field_code(field, indent + 8, module_index)
         yield i + f"    )"
     else:
         yield i+"    pass"
 
 
-def typedef_code(typedef: TypeDefDeclaration, indent=0):
+def typedef_code(cursor: Cursor, indent=0, module_index=None):
     i = indent * " "
-    yield i+f"{typedef.name} = {typedef.base_type.clang_type.spelling}"
+    name = ctypes_name_for_cursor(cursor)
+    base_type = ctypes_name_for_clang_type(cursor.underlying_typedef_type, module_index)
+    yield i+f"{name} = {base_type}"
 
 
-coder_map = {
-    StructDeclaration: struct_code,
-    TypeDefDeclaration: typedef_code,
+coder_for_cursor_kind = {
+    CursorKind.STRUCT_DECL: struct_code,
+    CursorKind.TYPEDEF_DECL: typedef_code,
 }
 
 __all__ = [
