dacfd2505007afcf772c94483f382f8988782b70
Christopher Bruns <cmbruns@rotatingpenguin.com>
2024-07-15 19:45:45 -0700

    Refactor typedef_tester

---
+++ tests/data/simple_typedef_ctypes_expected.py
@@ -0,0 +1,7 @@
+from ctypes import c_int
+
+IntAlias: type = c_int
+
+__all__ = [
+    "IntAlias",
+]
--- tests/typedef_tester.py
+++ tests/typedef_tester.py
@@ -1,33 +1,37 @@
-import inspect
+import ctypes
+import importlib.util
 import io
+import sys
 import unittest
 
 import wraptor
 
 
+def import_module_from_string(name: str, code: str):
+    spec = importlib.util.spec_from_loader(name, loader=None)
+    module = importlib.util.module_from_spec(spec)
+    exec(code, module.__dict__)
+    sys.modules[name] = module
+    globals()[name] = module
+    return module
+
+
 class TypedefTester(unittest.TestCase):
     def test_simple_typedef(self):
-        src = inspect.cleandoc("""
-            typedef int INT_TYPEDEF;
-        """)
-        expected = inspect.cleandoc("""
-            from ctypes import c_int
-            
-            INT_TYPEDEF: type = c_int
-            
-            __all__ = [
-                "INT_TYPEDEF",
-            ]
-        """) + "\n"
         mb = wraptor.ModuleBuilder(
-            path="foo.h",
-            unsaved_files=[("foo.h", src),]
+            path="data/simple_typedef_input.h",
         )
         mb.typedefs().include()
         cg = wraptor.CTypesCodeGenerator(mb)
-        result = io.StringIO()
-        cg.write_module(result)
-        self.assertEqual(expected, result.getvalue())
+        py_code_stream = io.StringIO()
+        cg.write_module(py_code_stream)
+        py_code = py_code_stream.getvalue()
+        with open("data/simple_typedef_ctypes_expected.py") as f:
+            py_code_expected = f.read()
+        self.assertEqual(py_code_expected, py_code)
+        # Verify the code is generated as expected
+        import_module_from_string("my_module", py_code)
+        self.assertEqual(ctypes.c_int, my_module.IntAlias)  # noqa
 
 
 if __name__ == "__main__":
